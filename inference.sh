5251af2a1d3918c7d01de30abcc4eeb8fd24971a
sang1583535 <e1583535@u.nus.edu>
2025-10-15 11:20:12 +0800

    improve code

--- inference.sh
+++ inference.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=nus-cs4248-project-mt
 #SBATCH --output=inference_mt_%j.out
 #SBATCH --gpus=1 # implicitly requests 1 GPUs
-#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 
@@ -28,22 +27,29 @@ mkdir -p outputs
 # --input-text "28岁厨师被发现死于旧金山一家商场" \
 
 # Or run inference on a file
+# Define paths. Adjust these paths as necessary.
+MODEL_PATH="$HOME/cs4248-project-mt/models/mt5-large-finetuned-single-gpu/checkpoint-19260"
+SRC_FILE="$HOME/cs4248-project-mt/dataset/tatoeba.zh"
+OUTPUT_FILE="$HOME/cs4248-project-mt/outputs/tatoeba_mt5_large.en"
+REF_FILE="$HOME/cs4248-project-mt/dataset/tatoeba.en"
+
 python3 inference.py \
---model-path $HOME/cs4248-project-mt/models/mt5-large-finetuned/checkpoint-310 \
---input-file $HOME/cs4248-project-mt/dataset/tatoeba.zh \
---output-file $HOME/cs4248-project-mt/outputs/tatoeba_mt5_large.en
+--model-path "$MODEL_PATH" \
+--input-file "$SRC_FILE" \
+--output-file "$OUTPUT_FILE" 
 
 # BLEU score 
 echo "Computing SACREBLEU score..."
-sacrebleu -tok 13a -w 2 $HOME/cs4248-project-mt/dataset/tatoeba.en < $HOME/cs4248-project-mt/outputs/tatoeba_mt5_large.en
+sacrebleu -tok 13a -w 2 $REF_FILE < $OUTPUT_FILE
 
 # COMET score
 echo "Computing COMET score..."
-comet-score -s $HOME/cs4248-project-mt/dataset/tatoeba.zh \
-    -t $HOME/cs4248-project-mt/outputs/tatoeba_mt5_large.en \
-    -r $HOME/cs4248-project-mt/dataset/tatoeba.en \
+comet-score -s $SRC_FILE \
+    -t $OUTPUT_FILE \
+    -r $REF_FILE \
     --batch_size 256 \
     --gpus 1 \
-    # --model $HOME/cs4248-project-mt/models/mt5-large-finetuned/checkpoint-310
+    --num_workers 16 \
+    --model_storage_path $MODEL_PATH
 
 echo "Inference complete at $(date)"
